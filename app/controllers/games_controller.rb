8385f208c94d29d34758727982b553984188b0d1
Lamia Belkacem <lamia.belkacem86@gmail.com>
2022-11-21 16:37:07 -0500

    challenge3

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,18 @@
+require 'open-uri'
 class GamesController < ApplicationController
   def new
     @letters = 10.times.map { [*'a'..'z'].sample }
   end
 
   def score
+    @letters = params[:initial_letters]
+    @opt = params[:word].split('').map do |char|
+      @letters.include? char
+    end
+    # parser
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    words_serialized = URI.parse(url).read
+    @words = JSON.parse(words_serialized)
+
   end
 end
